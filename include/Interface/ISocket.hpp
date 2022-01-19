57b0b57f61f3521a08752a62aa806fa5266627ab
Hugo PREVOST <hugo.prevost@epitech.eu>
2022-01-19 17:59:31 +0100

    [ADD]: network abstraction; (Interface) Socket, (Impl) Ip4

---
+++ include/Interface/ISocket.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-500-PAR-5-1-zia-marc.wang
+** File description:
+** ISocket
+*/
+
+#ifndef ISOCKET_HPP_
+#define ISOCKET_HPP_
+
+#include "Impl/Common.hpp"
+#include "Impl/Ip4.hpp"
+
+namespace aZIAt {
+    class ISocket {
+        public:
+            enum class Status {
+                Done,
+                Disconnect,
+                Error
+            };
+
+            virtual Status send(ByteArray &_data) = 0;
+
+            virtual Ip4 getIp() const = 0;
+            virtual Port getPort() const = 0;
+    };
+}
+
+#endif /* !ISOCKET_HPP_ */
