17c745c3a9d19ac9179b9d95797550cda86e1e47
Hugo PREVOST <hugo.prevost@epitech.eu>
2022-01-19 17:52:16 +0100

    [ADD]: general header with common ressource

---
+++ include/impl/Common.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-500-PAR-5-1-zia-marc.wang
+** File description:
+** Common
+*/
+
+#ifndef COMMON_HPP_
+#define COMMON_HPP_
+
+#include <array>
+#include <vector>
+
+#include <cstdint>
+
+namespace aZIAt {
+    using ByteArray = std::vector<uint8_t>;
+    using Flag = uint16_t;
+    using Uuid = std::array<char, 34>;
+
+    enum class MasterRequest {
+        List,
+        Info,
+        Load,
+        Unload
+    };
+
+    enum class Result : uint16_t {
+        Traitement,
+        FlagError,
+        ModuleError,
+        MasterError,
+        DoneNR,
+        Done
+    };
+}
+
+#endif /* !COMMON_HPP_ */
